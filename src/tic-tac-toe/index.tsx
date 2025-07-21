578ba30fe105825b19177e72b9d7c6e27dc30eca
Aditya Dixit <dixitaditya12@gmail.com>
2025-07-22 02:01:16 +0530

    lesson: how to swap styled-component with emotions

--- src/tic-tac-toe/index.tsx
+++ src/tic-tac-toe/index.tsx
@@ -1,15 +1,37 @@
 import React, { useState } from 'react'
 import {StyledGameWrap, StyledCell} from './style'
 
+const PLAYER = {
+    x: "X",
+    o:"O"
+}
+
 const TicTacToe = (props: TTicTacToe) => {
     const {row, col} = props
-    const [cells, setCells] = useState(Array(row*col).fill(""))
+    const [cells, setCells] = useState<string[]>(Array(row*col).fill(""))
+    const [player, setPlayer] = useState(PLAYER.x)
+
+
+    const handleClick = (index: number) =>{
+        console.log("clicked", index)
+    if(!cells[index]){
+        setCells(state=>{
+        const newCell = [...state]
+        newCell[index] = player
+        return newCell
+       })
+       setPlayer(player => player === PLAYER.x ? PLAYER.o : PLAYER.x)
+       console.log("clicked and set", index, cells)
+    }
+       
+    }
+
     return (
         <div className='full-page-center'>
             
             <StyledGameWrap row={row} col={col}>
                 {cells.map((cell, index)=>{
-                    return <StyledCell>{index+1}</StyledCell>
+                    return <StyledCell onClick={()=>handleClick(index)}>{cell}{index+1}</StyledCell>
                 })}
             </StyledGameWrap>
         </div>
--- src/tic-tac-toe/style.ts
+++ src/tic-tac-toe/style.ts
@@ -1,23 +1,26 @@
-import styled from "styled-components"
+import styled from "@emotion/styled"
 
 type IGrid = {
     row: number,
     col: number
 }
 
-export const StyledGameWrap = styled.div<IGrid>`
-    display: grid;
-    grid-template-rows: repeat(${props=>props.row}, 1fr);
-    grid-template-columns: repeat(${props=>props.col}, 1fr);
-`
+const _styledGAmeWrap = (props: IGrid)=>({
+    display: "grid",
+    gridTemplateRows: `repeat(${props.row}, 1fr)`,
+    gridTemplateColumns: `repeat(${props.col}, 1fr)`,
+})
 
-export const StyledCell = styled.div`
-    width: 50px;
-    height: 50px;
-    background: #ccc;
-    border: 1px solid #333;
-    display: flex;
-    align-items: center;
-    justify-content : center;
-    cursor: pointer;
-`
+ const _styledCell = {
+    width: 50,
+    height: 50,
+    background: "pink",
+    border: "1px solid #333",
+    display: "flex",
+    alignItems: "center",
+    justifyContent : "center",
+    cursor: "pointer",
+ }
+
+export const StyledGameWrap = styled.div(_styledGAmeWrap);
+export const StyledCell = styled.div(_styledCell);
